c706be9494488cd688a805d502ed6a94ac935878
Patricio Torres <patriciotcisternas95@gmail.com>
2025-04-18 19:51:55 -0400

    instalación tailwind

---
+++ app/tailwind/page.tsx
@@ -0,0 +1,5 @@
+const tailwind = () => {
+  return <div>Tailwind</div>;
+};
+
+export default tailwind;
---
+++ app/tailwind/working/page.tsx
@@ -0,0 +1,5 @@
+const working = () => {
+  return <h1 className="text-3xl font-bold">Tailwind funcionando!</h1>;
+};
+
+export default working;
