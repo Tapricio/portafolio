5fc8670d556e8e007e021c613dd9791299c8323e
Patricio Torres <patriciotcisternas95@gmail.com>
2025-04-18 20:03:00 -0400

    limpiando rama main

--- app/tailwind/page.tsx
+++
@@ -1,5 +0,0 @@
-const tailwind = () => {
-  return <div>Tailwind</div>;
-};
-
-export default tailwind;
--- app/tailwind/working/page.tsx
+++
@@ -1,5 +0,0 @@
-const working = () => {
-  return <h1 className="text-3xl font-bold">Tailwind funcionando!</h1>;
-};
-
-export default working;
