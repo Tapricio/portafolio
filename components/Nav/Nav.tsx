1eef5b8d7dff1d6a4450e52b5062dda3200b4c41
Patricio Torres <patriciotcisternas95@gmail.com>
2025-04-26 13:27:42 -0400

    navbar2

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,21 +1,21 @@
 "use client";
 import React from "react";
-import ToggleBtn from "./ToggleBtn";
-import { RxSun, RxMoon } from "react-icons/rx";
+/* import ToggleBtn from "./ToggleBtn";
+ */ import { RxSun, RxMoon } from "react-icons/rx";
 
 const Nav = () => {
   return (
     <nav className="w-[80%] h-[10vh] left-1/2 -translate-x-1/2 text-foreground flex justify-center fixed">
       <ul className="flex gap-7 text-3xl p-5">
-        {/* <li>
+        <li>
           <RxSun size={"auto"} />
         </li>
         <li>
           <RxMoon size={"auto"} />
-        </li> */}
-        <li>
-          <ToggleBtn sizeBtn={"40px"} />
         </li>
+        {/* <li>
+          <ToggleBtn sizeBtn={"3vw"} />
+        </li> */}
       </ul>
     </nav>
   );
